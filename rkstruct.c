8cdd7f1885c0ca2963b0d49511affacdaef01820
boonleng <boonleng@ou.edu>
2017-06-05 15:23:45 -0500

    Nomenclature following suggestions from python.org

--- rkstruct.c
+++ rkstruct.c
@@ -5,13 +5,13 @@
 #define IS_PY3    defined(PyModule_Create)
 
 // Wrappers
-static PyObject *PyRKInit(PyObject *self, PyObject *args, PyObject *keywords) {
+static PyObject *RKStructInit(PyObject *self, PyObject *args, PyObject *keywords) {
     RKSetWantScreenOutput(true);
     Py_INCREF(Py_None);
     return Py_None;
 }
 
-static PyObject *PyRKTest(PyObject *self, PyObject *args, PyObject *keywords) {
+static PyObject *RKStructTest(PyObject *self, PyObject *args, PyObject *keywords) {
     //RKShowTypeSizes();
     Py_buffer buffer;
 
@@ -24,28 +24,28 @@ static PyObject *PyRKTest(PyObject *self, PyObject *args, PyObject *keywords) {
     return result;
 }
 
-static PyObject *PyRKTestShowColors(PyObject *self, PyObject *args, PyObject *keywords) {
+static PyObject *RKStructTestShowColors(PyObject *self, PyObject *args, PyObject *keywords) {
     RKTestShowColors();
     Py_INCREF(Py_None);
     return Py_None;
 }
 
 // Standard boiler plates
-static PyMethodDef PyRKMethods[] = {
-    {"init",       (PyCFunction)PyRKInit,           METH_VARARGS | METH_KEYWORDS, "Init module"},
-    {"test",       (PyCFunction)PyRKTest,           METH_VARARGS | METH_KEYWORDS, "Test module"},
-    {"showColors", (PyCFunction)PyRKTestShowColors, METH_VARARGS | METH_KEYWORDS, "Color module"},
+static PyMethodDef RKStructMethods[] = {
+    {"init",       (PyCFunction)RKStructInit,           METH_VARARGS | METH_KEYWORDS, "Init module"},
+    {"test",       (PyCFunction)RKStructTest,           METH_VARARGS | METH_KEYWORDS, "Test module"},
+    {"showColors", (PyCFunction)RKStructTestShowColors, METH_VARARGS | METH_KEYWORDS, "Color module"},
     {NULL, NULL, 0, NULL}
 };
 
 #if IS_PY3
 
-static struct PyModuleDef PyRKModule = {
+static struct PyModuleDef RKStructModule = {
     PyModuleDef_HEAD_INIT,
     "rk",
     NULL,
     -1,
-    PyRKMethods
+    RKStructMethods
 };
 
 #endif
@@ -55,13 +55,13 @@ PyMODINIT_FUNC
 #if IS_PY3
 
 PyInit_rkstruct(void) {
-    return PyModule_Create(&PyRKModule);
+    return PyModule_Create(&RKStructModule);
 }
 
 #else
 
 initrkstruct(void) {
-    (void) Py_InitModule("rkstruct", PyRKMethods);
+    (void) Py_InitModule("rkstruct", RKStructMethods);
 }
 
 #endif
