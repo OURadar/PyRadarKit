c1b8a8d75e29e8b067704901b0a5c0ef091f30c2
boonleng <boonleng@ou.edu>
2018-06-15 10:13:23 -0500

    Moved all the string functions to misc.py

--- radarkit.py
+++ radarkit.py
@@ -19,6 +19,8 @@ import json
 
 import rkstruct as rk
 
+from misc import *
+
 # Some global variables / functions
 logger = logging.getLogger(__name__)
 version_info = rk.version()
@@ -53,25 +55,6 @@ class NETWORK_PACKET_TYPE:
     USER_SWEEP_DATA = 115
     USER_PRODUCT_DESCRIPTION = 116
 
-class COLOR:
-    reset = "\033[0m"
-    red = "\033[38;5;196m"
-    orange = "\033[38;5;214m"
-    yellow = "\033[38;5;226m"
-    lime = "\033[38;5;118m"
-    green = "\033[38;5;46m"
-    teal = "\033[38;5;49m"
-    iceblue = "\033[38;5;51m"
-    skyblue = "\033[38;5;39m"
-    blue = "\033[38;5;27m"
-    purple = "\033[38;5;99m"
-    indigo = "\033[38;5;201m"
-    hotpink = "\033[38;5;199m"
-    pink = "\033[38;5;213m"
-    salmon = "\033[38;5;210m"
-    python = "\033[38;5;226;48;5;24m"
-    radarkit = "\033[38;5;15;48;5;124m"
-
 # Generic functions
 def test(payload, debug=False):
     return rk.test(payload, debug=debug)
@@ -84,31 +67,6 @@ def read(filename, verbose=0):
         Read a sweep from a netcdf file
     """
     return rk.read(filename, verbose=0)
-
-def showArray(d, letter='U'):
-    formatDesc = ' {:6.2f}'
-    j = 0
-    print('    {} = [ {} ... {} ]'.format(colorize(letter, COLOR.yellow),
-                                              ' '.join([formatDesc.format(x) for x in d[j, :3]]),
-                                              ' '.join([formatDesc.format(x) for x in d[j, -3:]])))
-    for j in range(1, 3):
-        print('        [ {} ... {} ]'.format(' '.join([formatDesc.format(x) for x in d[j, :3]]),
-                                             ' '.join([formatDesc.format(x) for x in d[j, -3:]])))
-    print('        [  ...')
-    for j in range(-3, 0):
-        print('        [ {} ... {} ]'.format(' '.join([formatDesc.format(x) for x in d[j, :3]]),
-                                             ' '.join([formatDesc.format(x) for x in d[j, -3:]])))
-
-def colorize(string, color):
-    return '{}{}{}'.format(color, string, COLOR.reset)
-
-def variableInString(name, value):
-    if isinstance(value, (int, float)):
-        return '{}{}{} = {}{}{}'.format(COLOR.orange, name, COLOR.reset, COLOR.lime, value, COLOR.reset)
-    elif isinstance(value, (bool)):
-        return '{}{}{} = {}{}{}'.format(COLOR.orange, name, COLOR.reset, COLOR.purple, value, COLOR.reset)
-    else:
-        return '{}{}{} = {}{}{}'.format(COLOR.orange, name, COLOR.reset, COLOR.yellow, value, COLOR.reset)
 
 # An algorithm encapsulation
 class Algorithm(object):
